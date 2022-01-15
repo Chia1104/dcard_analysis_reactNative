577d37b4e5381aeb142478ef9f32d056422f7d6b
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-01-15 22:48:26 +0800

    createNativeStackNavigator

--- App.js
+++ App.js
@@ -8,12 +8,16 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import PostScreen from './src/screens/PostScreen';
 import ChartScreen from './src/screens/ChartScreen';
 import ChatScreen from './src/screens/ChatScreen';
-import SettingsScreen from './src/screens/SettingsScreen.js';
+import ProfileScreen from './src/screens/ProfileScreen.js';
 import HomeScreen from './src/screens/HomeScreen.js';
 import DcardDetailScreen from './src/screens/DcardDetailScreen.js';
 
 const Tab = createMaterialBottomTabNavigator();
 const PostStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const ChartStack = createNativeStackNavigator();
+const ChatStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -27,53 +31,86 @@ export default function App() {
             barStyle={{ backgroundColor: '#ffffff' }}
             screenOptions={{ headerShown: false }}>
             <Tab.Screen name="Home"
-                        component={HomeScreen}
                         options={{
                             tabBarLabel: 'Home',
                             tabBarIcon: ({ color, size }) => (
                                 <MaterialCommunityIcons name="home-outline" color={color} size={25} />
                             ),
-                        }}/>
-            <Tab.Screen name="Post" options={{
-                tabBarLabel: 'Post',
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialCommunityIcons name="card-text-outline" color={color} size={25} />
-                ),
-            }}>
-                {() => (
-                    <PostStack.Navigator>
-                        <PostStack.Screen
-                            name="Post"
-                            component={PostScreen}
-                        />
-                        <PostStack.Screen name="Detail" component={DcardDetailScreen} />
-                    </PostStack.Navigator>
-                )}
+                        }}>
+                        {() => (
+                            <HomeStack.Navigator>
+                                <HomeStack.Screen
+                                    name="Home"
+                                    component={HomeScreen}
+                                />
+                            </HomeStack.Navigator>
+                        )}
+            </Tab.Screen>
+            <Tab.Screen name="Post"
+                        options={{
+                            tabBarLabel: 'Post',
+                            tabBarIcon: ({ color, size }) => (
+                                <MaterialCommunityIcons name="card-text-outline" color={color} size={25} />
+                            ),
+                        }}>
+                            {() => (
+                                <PostStack.Navigator>
+                                    <PostStack.Screen
+                                        name="Post"
+                                        component={PostScreen}
+                                    />
+                                    <PostStack.Screen name="Detail" component={DcardDetailScreen} />
+                                </PostStack.Navigator>
+                            )}
             </Tab.Screen>
             <Tab.Screen name="Chart"
-                        component={ChartScreen}
                         options={{
                             tabBarLabel: 'Chart',
                             tabBarIcon: ({ color, size }) => (
                                 <MaterialCommunityIcons name="chart-donut" color={color} size={25} />
                             ),
-                        }}/>
+                        }}>
+                        {() => (
+                            <ChartStack.Navigator>
+                                <ChartStack.Screen
+                                    name="Chart"
+                                    component={ChartScreen}
+                                />
+                            </ChartStack.Navigator>
+                        )}
+            </Tab.Screen>
             <Tab.Screen name="Chat"
-                        component={ChatScreen}
                         options={{
                             tabBarLabel: 'Chat',
                             tabBarIcon: ({ color, size }) => (
                                 <MaterialCommunityIcons name="chat-processing-outline" color={color} size={25} />
                             ),
-                        }}/>
+                        }}>
+                {() => (
+                    <ChatStack.Navigator>
+                        <ChatStack.Screen
+                            name="Chat"
+                            component={ChatScreen}
+                        />
+                    </ChatStack.Navigator>
+                )}
+            </Tab.Screen>
             <Tab.Screen name="Profile"
-                        component={SettingsScreen}
                         options={{
                             tabBarLabel: 'Profile',
                             tabBarIcon: ({ color, size }) => (
                                 <MaterialCommunityIcons name="account-outline" color={color} size={25} />
                             ),
-                        }}/>
+                        }}>
+                {() => (
+                    <ProfileStack.Navigator>
+                        <ProfileStack.Screen
+                            name="Profile"
+                            component={ProfileScreen}
+                        />
+                    </ProfileStack.Navigator>
+                )}
+            </Tab.Screen>
         </Tab.Navigator>
       </NavigationContainer>
   );
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
-const SettingsScreen = () => {
+const ProfileScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Text>
@@ -11,4 +11,4 @@ const SettingsScreen = () => {
     );
 };
 
-export default SettingsScreen;
+export default ProfileScreen;
